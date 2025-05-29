ae18ae1b0df166ac2066a67da2470c901eb66be6
sashi-does <sashiontop@gmail.com>
2025-05-29 23:38:39 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,8 @@
 import { prisma } from "@repo/db/client"
 
 export default async function Page() {
-  const user = await prisma.user.findFirst({})
+
   return <div>
-      <h1>Hi There this is a test of CI/CD workflow</h1>
-      {user?.username}
-      {user?.password}
+      <h1>Hi There this is a test of CI/CD workflow - by sashi</h1>
   </div>
 }

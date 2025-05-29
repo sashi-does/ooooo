d1e2421168239371d279e07f60106940aece84f4
sashi-does <sashiontop@gmail.com>
2025-05-29 23:22:31 +0530

    added sample text to test pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@repo/db/client"
 export default async function Page() {
   const user = await prisma.user.findFirst({})
   return <div>
+      <h1>Hi There this is a test of CI/CD workflow</h1>
       {user?.username}
       {user?.password}
   </div>
